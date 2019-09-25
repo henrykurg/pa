17fbdd9ede96bd3ee5cd40b4133b818a46fa3983
Henry Kurg <kurg.henry@khk.ee>
2019-09-25 11:07:27 +0300

    Muujutaje defineermine

---
+++ 25.09.2019/variables.php
@@ -0,0 +1,23 @@
+<?php
+//muujuja defineerimine
+//$muutujuNimi=väärtus;
+
+// Väärtused - andmed - andmetüübid
+// Tekst - string - info, mis pannakse " või ' vahele.
+// Täisarvud - integer - 2, 7, 19, -15 jne
+// Reaalarvud - float - 2.5 , 8.2 jne
+
+$eesNimi = "Henry";
+$pereNimi = "Kurg";
+$email = "kurg.henry@khk.ee";
+$vanus = 28;
+$pikkus = 1.85;
+$kaal = 75;
+
+echo "Eesnimi: ".$eesNimi."<br>";
+echo "Perenimi: ".$pereNimi."<br>";
+echo "Email: " .$email."<br>";
+echo "Vanus: ".$vanus."<br>";
+echo "Pikkus: ".$pikkus."<br>";
+echo "Kaal: ".$kaal."<br>";
+
