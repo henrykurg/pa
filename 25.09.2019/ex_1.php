3aea8d2703287a511117ac64efe2d95c6a858598
Henry Kurg <kurg.henry@khk.ee>
2019-09-25 10:44:15 +0300

    Ülesanne 1 lahendus

---
+++ 25.09.2019/ex_1.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h1>Info minust</h1>";
+echo "eesnimi: Henry <br>";
+echo "perenimi: Kurg <br>";
+echo "email: <a href='mailto:kurg.henry@khk.ee'>kurg.henry@khk.ee<a/> <br>";
+echo "vanus: 27 <br>";
+echo "pikkus (m): 1.85<br>";
+echo "kaal (kg): 75<br>";
