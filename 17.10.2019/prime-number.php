e0ea45d6bbf36cce0dd713a37d37d4e3b0b3d215
Henry Kurg <kurg.henry@khk.ee>
2019-10-25 14:50:14 +0300

    20 esimese arvu algarvude summa

---
+++ 17.10.2019/prime-number.php
@@ -0,0 +1,24 @@
+<?php
+
+$sum = 0;
+for ($number = 1; $number <= 20; $number++) {
+    if ($number < 2) {
+        echo $number . ' - antud arv ei kuulu valitud vahemiku sisse <br>';
+    } else {
+        $divider = 2; // define divider
+        while ($number % $divider != 0) {
+            // while number is not divided by divider with modulo 0
+            $divider++; // get the next divider value
+        }
+        // if number and divider is equal - prime number
+        if ($number == $divider) {
+            echo $number . ' on algarv<br>';
+            $sum = $sum + $number; // $sum += $number;
+        } else {
+            // otherwise
+            echo $number . ' ei ole algarv<br>';
+        }
+    }
+}
+echo '<hr>';
+echo 'Antud vahemikus olevate algarvude summa on ' . $sum;
