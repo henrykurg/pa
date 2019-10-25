3b298046b677b25022e441cf4d0ec8cd549c34f0
Henry Kurg <kurg.henry@khk.ee>
2019-10-25 14:48:45 +0300

    pi arvutamine while abil

---
+++ 17.10.2019/pi.php
@@ -0,0 +1,19 @@
+<?php
+
+// define max count of elements
+$maxCount = 100000000;
+// define count for loop
+$count = 1;
+// define sum of elements
+$sum = 0;
+while ($count <= $maxCount) {
+    $element = 1 / (2 * $count - 1);
+    if ($count % 2 != 0) {
+        $sum = $sum + $element;
+    } else {
+        $sum = $sum - $element;
+    }
+    $count++;
+}
+echo 'Arvutatud pi = ' . ($sum * 4) . '<br>';
+echo 'Kontroll pi  = ' . pi();
